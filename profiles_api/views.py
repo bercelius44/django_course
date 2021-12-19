a9dd29163eda8c66333eee0d81e9b3eba99bed40
unknown <bercelius-44@hotmail.com>
2021-12-19 15:38:05 -0500

    Added login API

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,10 +1,11 @@
 from django.utils.translation import to_locale
 from rest_framework import authentication
-from rest_framework.serializers import Serializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, viewsets, filters
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 from profiles_api import serializers, models, permissions
 
@@ -97,3 +98,9 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.UpdateOwnProfile, )
     filter_backends = (filters.SearchFilter, )
     search_fields = ("name", "email", )
+
+
+class UserLoginApiView(ObtainAuthToken):
+    """Handle creating user auth Token"""
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+    
